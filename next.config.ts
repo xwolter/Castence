7a493fa888d4174c4ee2840b5c47536c40ef8728
Wolter <65672806+xwolter@users.noreply.github.com>
2025-12-10 17:15:16 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/proxy/:path*', // Ścieżka, której użyjesz w frontendzie
+                destination: 'https://api.rotify.pl/api/v1/:path*', // Prawdziwe API
+            },
+        ];
+    },
 };
 
 export default nextConfig;
